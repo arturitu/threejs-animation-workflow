cccb81b2ce666f9c648b06cfe314946db54cdd38
Unboring <arturo@unboring.net>
2016-08-24 15:51:44 +0200

    Add basic template to render a 3D model textured & exported with Blender

---
+++ js/main.js
@@ -0,0 +1,77 @@
+var clock, container, camera, scene, renderer, controls, listener;
+
+var ground, character;
+var light;
+var textureLoader = new THREE.TextureLoader();
+var loader = new THREE.JSONLoader();
+var isLoaded = false;
+
+init();
+
+function init () {
+  clock = new THREE.Clock();
+
+  scene = new THREE.Scene();
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  container = document.getElementById('container');
+  container.appendChild(renderer.domElement);
+
+  camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(0, 1.2, 2.5);
+  listener = new THREE.AudioListener();
+  camera.add(listener);
+
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.target = new THREE.Vector3(0, 0.6, 0);
+  // Lights
+  light = new THREE.AmbientLight(0xffffff, 1);
+  scene.add(light);
+
+  textureLoader.load('textures/ground.png', function (texture) {
+    var geometry = new THREE.PlaneBufferGeometry(2, 2);
+    geometry.rotateX(-Math.PI / 2);
+    var material = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
+    ground = new THREE.Mesh(geometry, material);
+    scene.add(ground);
+
+  });
+
+  loader.load('./models/eva-textured.json', function (geometry, materials) {
+    character = new THREE.Mesh(
+      geometry,
+      new THREE.MeshFaceMaterial(materials)
+    );
+
+    scene.add(character);
+
+    window.addEventListener('resize', onWindowResize, false);
+
+    animate();
+
+    isLoaded = true;
+  });
+
+}
+
+function onWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function animate () {
+  requestAnimationFrame(animate);
+  controls.update();
+  render();
+
+}
+
+function render () {
+  renderer.render(scene, camera);
+}
